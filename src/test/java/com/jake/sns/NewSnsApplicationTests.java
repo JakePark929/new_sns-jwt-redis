38e713ee246cbb9848bc7877ea612751c18294aa
epike <epikeran@gmail.com>
2023-03-21 16:16:47 +0900

    init: initialize project
    Web, Lombok, Security, JPA, Postgres DB 추가
    datasource autoconfig 무시 설정 했으므로 이후 변경 필요

---
+++ src/test/java/com/jake/sns/NewSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
